10a9d4188c41d7d615af723b3c7e7e7ad5b96014
Raito Muramatsu <55012572+raitolaito@users.noreply.github.com>
2020-09-25 10:41:25 +0900

    Add files via upload

---
+++ test_CREATE.php
@@ -0,0 +1,34 @@
+<html>
+<head><title>PDO CREATE</title></head>
+<body>
+
+<?php
+
+$dsn = 'mysql:dbname=co_19_264_99sv_coco_com;host=localhost;charset=utf8';
+$user = 'co-19-264.99sv-coco.c';
+$password = 'gJ4TjKuF';
+
+
+try {
+	// DBへ接続
+	$dbh = new PDO($dsn, $user, $password);
+
+	// SQL作成
+	$sql = 'CREATE TABLE contents (
+		id INT(11) AUTO_INCREMENT PRIMARY KEY,
+		name VARCHAR(20)
+	) engine=innodb default charset=utf8';
+
+	// SQL実行
+	$res = $dbh->query($sql);
+	
+
+} catch(PDOException $e) {
+
+	echo $e->getMessage();
+	die();
+}
+
+// 接続を閉じる
+$dbh = null;
+?>
