ac65916cb3f1b32c89cc8bdbe5d9469f12225717
Raito Muramatsu <55012572+raitolaito@users.noreply.github.com>
2020-09-30 16:19:55 +0900

    Update pdo_connect.php

--- pdo_connect.php
+++ pdo_connect.php
@@ -1,8 +1,8 @@
 <?php
 
-$dsn = 'mysql:dbname=co_19_264_99sv_coco_com;host=localhost';
-$user = 'co-19-264.99sv-coco.c';
-$password = 'gJ4TjKuF';
+$dsn = 'mysql:**********************;host=localhost';
+$user = '************************';
+$password = '***********';
 
 try{
     $dbh = new PDO($dsn, $user, $password);
